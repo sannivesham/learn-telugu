69aae43e7173af9c94e62f3df3816ad4f642d764
sannivesham <web.sannivesham@gmail.com>
2026-08-01 23:47:42 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,16 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signOut,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAjWX_v7QgLMd_ypufFiyFItWj1JA8TFGw",
   authDomain: "aksharam-app.firebaseapp.com",
@@ -7,4 +19,20 @@ const firebaseConfig = {
   messagingSenderId: "176028549003",
   appId: "1:176028549003:web:95c135812d6ce55902798e",
   measurementId: "G-2BRP7FTB54"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export {
+  auth,
+  provider,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signOut,
+  onAuthStateChanged
 };
