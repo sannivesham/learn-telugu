af3009111c49e8ea2aac2b6680e71d88c75d0deb
sannivesham <web.sannivesham@gmail.com>
2026-08-01 23:44:22 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAjWX_v7QgLMd_ypufFiyFItWj1JA8TFGw",
+  authDomain: "aksharam-app.firebaseapp.com",
+  projectId: "aksharam-app",
+  storageBucket: "aksharam-app.firebasestorage.app",
+  messagingSenderId: "176028549003",
+  appId: "1:176028549003:web:95c135812d6ce55902798e",
+  measurementId: "G-2BRP7FTB54"
+};
